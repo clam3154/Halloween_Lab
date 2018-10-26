f98bd91000d76ad0961d1c0b4eafdc553a203f65
clam3154 <clam3154@bths.edu>
2018-10-26 10:17:28 -0400

    Initial commit

---
+++ src/JackOLantern.java
@@ -0,0 +1,27 @@
+public class JackOLantern {
+    private String[][] faceFeatures ;
+    public JackOLantern(String[][] faceFeatures)
+    {
+        this.faceFeatures = faceFeatures;
+    }
+
+    public static void edit(String replace, int row, int column)
+    {
+
+    }
+
+    public String toString()
+    {
+        String row = "";
+
+        for(int i=0; i<faceFeatures.length; i++)
+        {
+            for(int j=0; j<faceFeatures[i].length; j++)
+            {
+                row += faceFeatures[i][j];
+            }
+            row = "";
+        }
+        return row;
+    }
+}
---
+++ src/Runner.java
@@ -0,0 +1,14 @@
+public class Runner {
+    public void main(String[] args)
+    {
+        String[][] faceFeatures = new String[][]{
+                {" ","X","X","X","X","X","X"," "},
+                {"X","X","X","X","X","X","X","X"},
+                {"X","X","X","X","X","X","X","X"},
+                {"X","X","X","X","X","X","X","X"},
+                {"X","X","X","X","X","X","X","X"},
+                {" ","X","X","X","X","X","X"," "}
+        };
+        System.out.println(toString());
+    }
+}
